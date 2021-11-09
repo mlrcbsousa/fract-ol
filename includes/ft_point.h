4eb2840de1b268e08e543663b619829a705238d6
Manuel Sousa <mlrcbsousa@gmail.com>
2021-11-09 22:27:48 +0100

    Norm checks includes/ft_point.h

--- includes/ft_point.h
+++ includes/ft_point.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_point.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: msousa <msousa@student.42.fr>              +#+  +:+       +#+        */
+/*   By: msousa <mlrcbsousa@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/29 16:29:05 by msousa            #+#    #+#             */
-/*   Updated: 2021/11/04 21:02:20 by msousa           ###   ########.fr       */
+/*   Updated: 2021/11/09 22:27:42 by msousa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,6 @@ struct	s_point
 
 typedef struct s_point	t_point;
 
-t_point point_add(t_point a, t_point b);
+t_point	point_add(t_point a, t_point b);
 
 #endif
