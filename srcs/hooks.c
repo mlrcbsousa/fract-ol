b957a770a7c636e7ff185d107e97e421a3807c2b
Manuel Sousa <mlrcbsousa@gmail.com>
2021-11-09 22:16:12 +0100

    Norm checks srcs/hooks.c

--- srcs/hooks.c
+++ srcs/hooks.c
@@ -6,37 +6,37 @@
 /*   By: msousa <mlrcbsousa@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/07 16:01:20 by msousa            #+#    #+#             */
-/*   Updated: 2021/11/09 21:25:15 by msousa           ###   ########.fr       */
+/*   Updated: 2021/11/09 22:16:01 by msousa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-static int close_app(t_app *self)
+static int	close_app(t_app *self)
 {
 	mlx_destroy_window(self->mlx, self->mlx_window);
 	exit(0);
 }
 
-static void move(int key, t_app *self)
+static void	move(int key, t_app *self)
 {
 	if (key == ARROW_RIGHT || key == ARROW_LEFT)
 		self->axis_x = range_move(self->axis_x,
-															range_size(self->axis_x) * 0.05 
-															* ft_ternary(key == ARROW_RIGHT, 1, -1));
+				range_size(self->axis_x) * 0.05
+				* ft_ternary(key == ARROW_RIGHT, 1, -1));
 	else if (key == ARROW_UP || key == ARROW_DOWN)
 		self->axis_y = range_move(self->axis_y,
-															range_size(self->axis_y) * 0.05 
-															* ft_ternary(key == ARROW_DOWN, 1, -1));
+				range_size(self->axis_y) * 0.05
+				* ft_ternary(key == ARROW_DOWN, 1, -1));
 	draw(self);
 }
 
-static int key_hook(int key, t_app *self)
+static int	key_hook(int key, t_app *self)
 {
 	if (key == KEY_ESC)
 		close_app(self);
-	else if (key == ARROW_UP || key == ARROW_DOWN || key == ARROW_LEFT ||
-					 key == ARROW_RIGHT)
+	else if (key == ARROW_UP || key == ARROW_DOWN || key == ARROW_LEFT
+		|| key == ARROW_RIGHT)
 		move(key, self);
 	else if (key == KEY_C)
 	{
@@ -46,7 +46,7 @@ static int key_hook(int key, t_app *self)
 	return (0);
 }
 
-static int zoom(int button, int x, int y, t_app *self)
+static int	zoom(int button, int x, int y, t_app *self)
 {
 	double	ratio;
 	double	x_mapped;
@@ -71,7 +71,7 @@ static int zoom(int button, int x, int y, t_app *self)
 	return (0);
 }
 
-void set_hooks(t_app *self)
+void	set_hooks(t_app *self)
 {
 	mlx_key_hook(self->mlx_window, key_hook, self);
 	mlx_hook(self->mlx_window, BUTTON_PRESS, BUTTON_PRESS_MASK, zoom, self);
