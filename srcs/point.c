63419b11b377f27963d11957cb754fd95f0afe65
Manuel Sousa <mlrcbsousa@gmail.com>
2021-11-09 22:11:33 +0100

    Norm checks srcs/point.c

--- srcs/point.c
+++ srcs/point.c
@@ -6,24 +6,24 @@
 /*   By: msousa <mlrcbsousa@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 12:12:36 by msousa            #+#    #+#             */
-/*   Updated: 2021/11/08 09:57:11 by msousa           ###   ########.fr       */
+/*   Updated: 2021/11/09 22:11:16 by msousa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-t_point point_add(t_point a, t_point b)
+t_point	point_add(t_point a, t_point b)
 {
-	t_point result;
+	t_point	result;
 
 	result.x = a.x + b.x;
 	result.y = a.y + b.y;
 	return (result);
 }
 
-t_point complex_squared(t_point point)
+t_point	complex_squared(t_point point)
 {
-	t_point result;
+	t_point	result;
 
 	result.x = pow(point.x, 2.0) - pow(point.y, 2.0);
 	result.y = 2.0 * point.x * point.y;
