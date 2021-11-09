033fad3343bae30576c6849c2feeb9ad1d64d123
Manuel Sousa <mlrcbsousa@gmail.com>
2021-11-09 22:13:51 +0100

    Norm checks srcs/range.c

--- srcs/range.c
+++ srcs/range.c
@@ -6,7 +6,7 @@
 /*   By: msousa <mlrcbsousa@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 12:47:39 by msousa            #+#    #+#             */
-/*   Updated: 2021/11/09 20:00:33 by msousa           ###   ########.fr       */
+/*   Updated: 2021/11/09 22:13:41 by msousa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,14 @@ double	range_map(double n, t_range from, t_range to)
 	return (to.min + (n / range_size(from) * range_size(to)));
 }
 
-t_range range_move(t_range range, double n)
+t_range	range_move(t_range range, double n)
 {
 	range.max = range.max + n;
 	range.min = range.min + n;
 	return (range);
 }
 
-t_range range_resize(t_range range, double ratio)
+t_range	range_resize(t_range range, double ratio)
 {
 	range.max = range.max * ratio;
 	range.min = range.min * ratio;
